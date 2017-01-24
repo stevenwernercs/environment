da6f7ef7846e2b25114cd42331ece65e1febae2b
Steven Werner <swerner@trifidearth.com>
2017-01-23 20:51:59 -0600

    initial centos base setup

---
+++ setup/IDE/netbeans-8.0.1-linux.sh
@@ -0,0 +1,6 @@
+
+#http://www.itzgeek.com/how-tos/linux/centos-how-tos/install-netbeans-ide-8-0-1-on-centos-7-rhel-7.html
+wget http://download.netbeans.org/netbeans/8.0.1/final/bundles/netbeans-8.0.1-linux.sh
+
+sh netbeans-8.0.1-linux.sh
+
---
+++ setup/browsers/chrome/install-chrome.sh
@@ -0,0 +1,5 @@
+## steps pulled from https://www.cyberciti.biz/faq/howto-install-google-chrome-on-redhat-rhel-fedora-centos-linux/
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo yum install ./google-chrome-stable_current_*.rpm
+google-chrome &
