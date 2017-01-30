0d7addaa3f01693fb529e15c6c17ab3555b1cdb6
Steven Werner <stevenWernerCS@gmail.com>
2017-01-29 23:14:52 -0600

    updating new tools and tips

--- setup/yum-repos/setRepos.sh
+++ setup/yum-repos/setRepos.sh
@@ -4,3 +4,7 @@ REPO_SRC_DIR="$(readlink -fn "$(dirname "${0}")")"
 REPO_DEST_DIR="/etc/yum.repos.d"
 
 sudo cp -iv "$REPO_SRC_DIR/"*.repo "$REPO_DEST_DIR"
+
+echo "also check out these repos: https://wiki.centos.org/AdditionalResources/Repositories"
+
+echo "sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm"
