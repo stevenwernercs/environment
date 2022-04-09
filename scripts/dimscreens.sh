26cf9bba361a3e3dbf6d8dc7bf5627fe72e1d6df
Steven Werner <stevenWernerCS@gmail.com>
2022-04-09 13:30:06 -0500

    added gcc

--- scripts/dimscreens.sh
+++ scripts/dimscreens.sh
@@ -4,9 +4,9 @@ if (( $(bc <<< "$1 >= 0.2") )) && (( $(bc <<< "$1 <= 1.0") ))
 then
   BRIGHTNESS="$1"
   xrandr --output DVI-I-1 --brightness "$BRIGHTNESS"
+  xrandr --output HDMI-1 --brightness "$BRIGHTNESS"
   BRIGHTNESS=$(bc <<< "$BRIGHTNESS-0.05")
   xrandr --output DVI-I-2 --brightness "$BRIGHTNESS"
-  xrandr --output HDMI-1 --brightness "$BRIGHTNESS"
 else
   >&2 echo "Not gonna use $1, 0.2 <= try >= 1.0"
   exit 1
